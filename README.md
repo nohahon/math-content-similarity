f6159277b05f577196ea40a02650ee133cb9db51
AnkitSatpute <ankitsureshsatpute@gmail.com>
2023-10-05 13:31:24 +0200

    added initial code and docs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# MathContentSimil
+# MathContentSimillarity 
+
 Experiments with LLMs for finding math content similarity
+
+Related articles/papers:
+
+1. [Parameter Efficient Fine Tuning (PEFT)](https://ojs.aaai.org/index.php/AAAI/article/view/26505/26277)
+2. LORA: Low Rank Adaptation of Large Language Models(https://arxiv.org/pdf/2106.09685.pdf)
+3. [Dataset on huggingface](https://huggingface.co/docs/datasets/create_dataset) 
+4. [Article on Example fine tuning using PEFT]https://towardsdatascience.com/fine-tuning-large-language-models-llms-23473d763b91
---
+++ fineTuningInfo/code/generateEMbedds.py
@@ -0,0 +1,40 @@
+import argparse
+import numpy as np
+from llama_cpp import Llama
+
+def testGenEmbedds():
+	parser = argparse.ArgumentParser()
+	# Default model downloaded and saved in local folder
+	parser.add_argument("-m", "--model", type=str, default="models/13b/ggml-model.bin")
+	args = parser.parse_args()
+	llm = Llama(model_path=args.model, embedding=True)
+	print(llm.create_embedding("Hello world!"))
+
+def cosine_similarity(vector1, vector2):
+	#Calculates the cosine similarity of two vectors.
+	dot_product = np.dot(vector1, vector2)
+	norm1 = np.linalg.norm(vector1)
+	norm2 = np.linalg.norm(vector2)
+	cosine_similarity = dot_product / (norm1 * norm2)
+	return cosine_similarity
+
+def loopThrgandGetEmbedd(loc_json):
+
+	# add trained model
+	llm = Llama(model_path="models_lamma/codellama-7b.Q6_K.gguf", embedding=True)
+    # tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
+    # model = AutoModel.from_pretrained('allenai/scibert_scivocab_uncased')
+
+    getlistofjsonl = os.listdir(loc_json)
+    for eachJSONL in getlistofjsonl:
+        print("Saving embeddings for: ",eachJSONL)
+        dictHere = dict()
+        with jsonlines.open(loc_json+'/'+eachJSONL, 'r') as json_file:
+            for eachLine in json_file:
+                dictHere[eachLine["docID"]] = llm.create_embedding(eachLine["reviews"])
+                # print(embeddings)
+        np.save(eachJSONL[:-6]+".npy", dictHere)
+        sys.exit(0)
+
+
+loopThrgandGetEmbedd("C:/Users/asa/Desktop/Projects/22Math_recSys/data/Final_All/allWithComplexity")
