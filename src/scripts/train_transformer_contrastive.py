d5ff8510efcdb32df094322d0cda245b0179b20c
Tomas H <horychtomas98@gmail.com>
2023-10-17 20:33:09 +0900

    added artifact download

--- src/scripts/train_transformer_contrastive.py
+++ src/scripts/train_transformer_contrastive.py
@@ -1,19 +1,18 @@
 from src.models.transformer_model import Transformer
 from src.models.contrastive_loss import ContrastiveLoss
 
-from transformers import Trainer, TrainingArguments
+from transformers import Trainer, TrainingArguments, DataCollatorWithPadding
 import pandas as pd
 from src.wandb_.wandb_client import WandbClient
 import wandb
 
-d = pd.read_csv(
-    "/home/tomas/Documents/MBG/projects/AndreAnkitMath/MathContentSimilarity/data/final_train_dataset.csv",
-)[:5]
+wandbc = WandbClient("test") 
+dataset = pd.read_csv(wandbc.load_dataset('final_train_dataset'))
+
 embedding_model = Transformer("distilbert-base-uncased")
-tokenized = embedding_model.tokenize(d)
+tokenized = embedding_model.tokenize(dataset)
 loss_model = ContrastiveLoss(embedding_model)
 
-wbclient = WandbClient("test")
 training_args = TrainingArguments(
     report_to="wandb",
     output_dir="./",
@@ -23,19 +22,16 @@ training_args = TrainingArguments(
     logging_steps=1,
     disable_tqdm=False,
     weight_decay=0.1,
-    warmup_steps=10,
+    warmup_steps=1,
     learning_rate=4e-5,
     run_name="test",
 )
-trainer = Trainer(loss_model, training_args, train_dataset=tokenized)
+trainer = Trainer(loss_model, training_args, train_dataset=tokenized,tokenizer=embedding_model.tokenizer,data_collator=DataCollatorWithPadding(tokenizer=embedding_model.tokenizer))
 trainer.train()
-wbclient.upload_dataset(
-    "/home/tomas/Documents/MBG/projects/AndreAnkitMath/MathContentSimilarity/data/final_train_dataset.csv",
-)
 # loss_model.model.save('./first.pth')
 
 # best_model = wandb.Artifact(f"model_{run.id}", type="model")
 # best_model.add_file("my_model.h5")
 # run.log_artifact(best_model)
 
-wbclient.finish()
+wandbc.finish()
--- src/wandb_/wandb_client.py
+++ src/wandb_/wandb_client.py
@@ -1,12 +1,15 @@
 from config import PROJECT_NAME
 import wandb
 from src.myutils import get_dataset_name
+import os
 
 
 class WandbClient:
     def __init__(self, run_name=str):
         self.project_name = PROJECT_NAME
         self.run = wandb.init(project=self.project_name, name=run_name)
+        self.artifacts_path = os.path.join(self.run.entity,self.project_name)
+        # self
 
     def upload_model(self, pt_model):
         artifact = wandb.Artifact(name="first_attempt", type="model")
@@ -20,6 +23,11 @@ class WandbClient:
         )
         artifact.add_file(local_path=dataset, name=get_dataset_name(dataset))
         self.run.log_artifact(artifact)
+
+    def load_dataset(self,dataset_name):
+        artifact = self.run.use_artifact(os.path.join(self.artifacts_path,dataset_name+':latest'), type='dataset')
+        artifact_dir = artifact.download()
+        return os.path.join(artifact_dir,dataset_name)
 
     def finish(self):
         wandb.finish()
