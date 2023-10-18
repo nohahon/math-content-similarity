77e85fb6b94895c4e6477208109a98b8f1eb9707
Tomas H <horychtomas98@gmail.com>
2023-10-18 10:41:04 +0900

    updated training script

--- src/scripts/train_transformer_contrastive.py
+++ src/scripts/train_transformer_contrastive.py
@@ -4,34 +4,42 @@ from src.models.contrastive_loss import ContrastiveLoss
 from transformers import Trainer, TrainingArguments, DataCollatorWithPadding
 import pandas as pd
 from src.wandb_.wandb_client import WandbClient
-import wandb
 
-wandbc = WandbClient("test") 
-dataset = pd.read_csv(wandbc.load_dataset('final_train_dataset'))
+wandbc = WandbClient("test")
+train_dataset = pd.read_csv(wandbc.load_dataset("final_train_dataset"))
+dev_dataset = pd.read_csv(wandbc.load_dataset("final_dev_dataset"))
 
-embedding_model = Transformer("distilbert-base-uncased")
-tokenized = embedding_model.tokenize(dataset)
+
+embedding_model = Transformer("BAAI/llm-embedder")
+tokenized_train = embedding_model.tokenize(train_dataset)
+tokenized_dev = embedding_model.tokenize(dev_dataset)
+
 loss_model = ContrastiveLoss(embedding_model)
 
 training_args = TrainingArguments(
     report_to="wandb",
     output_dir="./",
     overwrite_output_dir=True,
+    per_device_eval_batch_size=4,
+    per_device_train_batch_size=4,
     num_train_epochs=2,
-    per_device_train_batch_size=3,
-    logging_steps=1,
+    do_eval=True,
+    evaluation_strategy="steps",
+    logging_steps=20,
+    eval_steps=20,
     disable_tqdm=False,
     weight_decay=0.1,
     warmup_steps=1,
     learning_rate=4e-5,
     run_name="test",
 )
-trainer = Trainer(loss_model, training_args, train_dataset=tokenized,tokenizer=embedding_model.tokenizer,data_collator=DataCollatorWithPadding(tokenizer=embedding_model.tokenizer))
+trainer = Trainer(
+    loss_model,
+    training_args,
+    train_dataset=tokenized_train,
+    eval_dataset=tokenized_dev,
+    tokenizer=embedding_model.tokenizer,
+    data_collator=DataCollatorWithPadding(tokenizer=embedding_model.tokenizer),
+)
 trainer.train()
-# loss_model.model.save('./first.pth')
-
-# best_model = wandb.Artifact(f"model_{run.id}", type="model")
-# best_model.add_file("my_model.h5")
-# run.log_artifact(best_model)
-
 wandbc.finish()
---
+++ src/scripts/upload_dataset.py
@@ -0,0 +1,8 @@
+from src.wandb_.wandb_client import WandbClient
+
+wandbc = WandbClient("upload")
+wandbc.upload_dataset("data/final/final_train_dataset.csv")
+wandbc.upload_dataset("data/final/final_dev_dataset.csv")
+wandbc.upload_dataset("data/final/final_test_dataset.csv")
+
+wandbc.finish()
