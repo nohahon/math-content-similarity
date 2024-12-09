874c9b477baec824bd9914794914b0a0c947ec36
ankit <ankit@zbmath.org>
2024-12-09 11:27:50 +0100

    generating plots for all epochs

--- src/zbCitReviwRecmn/stella/generate_plots.py
+++ src/zbCitReviwRecmn/stella/generate_plots.py
@@ -1,18 +1,23 @@
+import os
 import torch
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Load the pickle file containing the training history (list of tuples)
-pickle_file_path = 'loss_acc/5e_6_clip/loss_acc_3.pkl'  # Adjust this to your pickle file path
+pickle_folder_path = 'loss_acc/5e_6/'
+pickle_files = [f for f in os.listdir(pickle_folder_path) if f.endswith('.pkl')]
 
-# Read the pickle file
-with open(pickle_file_path, 'rb') as f:
-    training_history = pickle.load(f)
+print(pickle_files)
 
-# Extract loss and accuracy values from the list of tuples
-loss_values = [item[1] for item in training_history]  # First element is loss
-accuracy_values = [item[0] for item in training_history]  # Second element is accuracy
+loss_values = []
+accuracy_values = []
+for pickle_file in pickle_files:
+    pickle_file_path = os.path.join(pickle_folder_path, pickle_file)
+    with open(pickle_file_path, 'rb') as f:
+        training_history = pickle.load(f)
+    loss_values.extend([item[1] for item in training_history])  # First element is loss
+    accuracy_values.extend([item[0] for item in training_history])  # Second element is accuracy
 
 # Ensure that any tensor is moved to the CPU, detached, and converted to numpy
 loss_values = [loss.detach().cpu().numpy() if isinstance(loss, torch.Tensor) else loss for loss in loss_values]
@@ -29,9 +34,9 @@ batches_accuracy = []
 for i in range(num_batches):
     batch_loss = np.mean(loss_values[i * batch_size: (i + 1) * batch_size])
     batch_accuracy = np.mean(accuracy_values[i * batch_size: (i + 1) * batch_size])
+
     batches_loss.append(batch_loss)
     batches_accuracy.append(batch_accuracy)
-
 
 # Plotting Loss vs. Batch
 plt.figure(figsize=(20, 6))
@@ -40,7 +45,7 @@ plt.xlabel('Batch')
 plt.ylabel('Average Loss')
 plt.title('Average Loss per Batch')
 plt.tight_layout()
-plt.savefig('plots_loss_acc/average_loss_ep2_5e_6_clip.png')
+plt.savefig('plots_loss_acc/5e_6/average_loss_ep2_5e_6_compl.png')
 plt.close()
 
 # Plotting Accuracy vs. Batch
@@ -50,7 +55,7 @@ plt.xlabel('Batch')
 plt.ylabel('Average Accuracy')
 plt.title('Average Accuracy per Batch')
 plt.tight_layout()
-plt.savefig('plots_loss_acc/average_acc_ep2_5e_6_clip.png')
+plt.savefig('plots_loss_acc/5e_6/average_acc_ep2_5e_6_compl.png')
 plt.close()
 
 # Optionally, display the plots
